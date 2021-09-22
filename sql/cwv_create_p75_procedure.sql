a3b58d25a5a263717539e3e6ed03b376d2c6de56
Adam Read <adamread@google.com>
2021-09-22 10:54:38 +0200

    Adding stored procedure for finding p75 values.
    This stored procedure will be added to the GA dataset and used by the
    altering script to retrieve the CWV p75 values. This is to avoid
    embedding SQL in the script.
    
    Change-Id: I037a4cc5e42028435b468e1b901425988386b611

---
+++ sql/cwv_create_p75_procedure.sql
@@ -0,0 +1,15 @@
+-- Creates a stored procedure that retrieves the p75 values for the given date
+-- range.
+
+CREATE OR REPLACE
+  PROCEDURE analytics_ < GA_ID >.get_cwv_p75_for_date(start_date date, num_days INT64) BEGIN
+SELECT
+  metric_name, APPROX_QUANTILES(metric_value, 100)[OFFSET(75)] AS p75, COUNT(1) AS count
+FROM `<PROJECT_ID>.analytics_<GA_ID>.web_vitals_summary`
+WHERE
+  PARSE_DATE('%Y%m%d', event_date)
+  BETWEEN DATE_SUB(start_date, INTERVAL num_days DAY)
+  AND DATE_SUB(start_date, INTERVAL 1 DAY)
+GROUP BY 1;
+
+END
