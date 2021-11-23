9f313997de9dfd2e10265e04e2c8a6ed3428dbfc
Adam Read <adamread@google.com>
2021-11-23 10:24:47 +0100

    Completed install script, fixed bug in SQL, and added license.
    Completed the installation script. It's now deploying the Cloud Run function
    and creating the eventarc trigger. The code was split into functions and
    cleaned up to match go/shellstyle.
    
    Backticks were added to the materialized table name to ensure project names
    with dashes are properly handled.
    
    The license header was added where missing.
    
    Change-Id: Ibdb79ed5f8ee7d65a5a6b4c4fe6a521f5e9053d6

--- notifications/main.go
+++ notifications/main.go
@@ -1,12 +1,28 @@
-// Provides a Google Cloud Run function that sends email notifications if Core
-// Web Vital (CWV) values drop below a defined threshold.
-//
-// The Core Web Vitals are a set of metrics designed to measure the performance
-// of websites based on user experience. They are
-//   - Largest Contentful Paint (LCP)
-//   - Cumulative Layout Shift (CLS)
-//   - First Input Delay (FID)
-// For more information on CWV, see https://web.dev/vitals
+/*
+ * Copyright 2021 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Provides a Google Cloud Run function that sends email notifications if Core
+ * Web Vital (CWV) values drop below a defined threshold.
+ *
+ * The Core Web Vitals are a set of metrics designed to measure the performance
+ * of websites based on user experience. They are
+ *   - Largest Contentful Paint (LCP)
+ *   - Cumulative Layout Shift (CLS)
+ *   - First Input Delay (FID)
+ * For more information on CWV, see https://web.dev/vitals
+ */
+
 package main
 
 import (
