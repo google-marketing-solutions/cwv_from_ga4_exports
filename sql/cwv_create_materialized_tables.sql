293234ce26549bef1301e0cfb3a6fbce97a23dda
Adam Read <adamread@google.com>
2021-11-23 14:55:29 +0100

    Deploying SQL for materialized table now schedules the query too.
    Updated the install script so that the query to materialize the CWV summary
    data is scheduled every 24 hours. Had to cat the contents of the file due to bq
    needing the user to paste a version_info string into the console. A redirect
    results in the console getting an EOF and dying.
    
    Changed all of the single-quotes to double in the SQL.
    
    Added the license to the create_p75 SQL script.
    
    Change-Id: I9a5ee86cdfb1480c585c07de8e143ac867605005

--- sql/cwv_create_materialized_tables.sql
+++ sql/cwv_create_materialized_tables.sql
@@ -21,15 +21,15 @@ AS
 SELECT
   ga_session_id,
   IF(
-    EXISTS(SELECT 1 FROM UNNEST(events) AS e WHERE e.event_name = 'first_visit'),
-    'New user',
-    'Returning user') AS user_type,
+    EXISTS(SELECT 1 FROM UNNEST(events) AS e WHERE e.event_name = "first_visit"),
+    "New user",
+    "Returning user") AS user_type,
   IF(
-    (SELECT MAX(session_engaged) FROM UNNEST(events)) > 0, 'Engaged', 'Not engaged')
+    (SELECT MAX(session_engaged) FROM UNNEST(events)) > 0, "Engaged", "Not engaged")
     AS session_engagement,
   evt.* EXCEPT (session_engaged, event_name),
   event_name AS metric_name,
-  FORMAT_TIMESTAMP('%Y%m%d', event_timestamp) AS event_date
+  FORMAT_TIMESTAMP("%Y%m%d", event_timestamp) AS event_date
 FROM
   (
     SELECT
@@ -51,16 +51,16 @@ FROM
             event_timestamp,
             event_name,
             metric_id,
-            IF(event_name = 'LCP', metric_value / 1000, metric_value) AS metric_value,
+            IF(event_name = "LCP", metric_value / 1000, metric_value) AS metric_value,
             user_pseudo_id,
             session_engaged,
             session_revenue) AS custom_event
         FROM
           (
             SELECT
-              (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id')
+              (SELECT value.int_value FROM UNNEST(event_params) WHERE key = "ga_session_id")
                 AS ga_session_id,
-              (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'metric_id')
+              (SELECT value.string_value FROM UNNEST(event_params) WHERE key = "metric_id")
                 AS metric_id,
               ANY_VALUE(device.category) AS device_category,
               ANY_VALUE(device.operating_system) AS device_os,
@@ -69,10 +69,10 @@ FROM
               ANY_VALUE(traffic_source.source) AS traffic_source,
               ANY_VALUE(
                 REGEXP_SUBSTR(
-                  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location'),
-                  r'^[^?]+')) AS page_path,
+                  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = "page_location"),
+                  r"^[^?]+")) AS page_path,
               ANY_VALUE(
-                (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'debug_target'))
+                (SELECT value.string_value FROM UNNEST(event_params) WHERE key = "debug_target"))
                 AS debug_target,
               ANY_VALUE(user_pseudo_id) AS user_pseudo_id,
               ANY_VALUE(geo.country) AS country,
@@ -84,20 +84,20 @@ FROM
                     COALESCE(
                       value.double_value, value.int_value, CAST(value.string_value AS NUMERIC))
                   FROM UNNEST(event_params)
-                  WHERE key = 'session_engaged'
+                  WHERE key = "session_engaged"
                 )) AS session_engaged,
               TIMESTAMP_MICROS(MAX(event_timestamp)) AS event_timestamp,
               MAX(
                 (
                   SELECT COALESCE(value.double_value, value.int_value)
                   FROM UNNEST(event_params)
-                  WHERE key = 'metric_value'
+                  WHERE key = "metric_value"
                 )) AS metric_value,
             FROM
               # Replace source table name
               `<PROJECT_ID>.analytics_<GA_ID>.events_*`
             WHERE
-              event_name IN ('LCP', 'FID', 'CLS', 'first_visit', 'purchase')
+              event_name IN ("LCP", "FID", "CLS", "first_visit", "purchase")
             GROUP BY
               1, 2
           )
@@ -107,4 +107,4 @@ FROM
     GROUP BY ga_session_id
   )
 CROSS JOIN UNNEST(events) AS evt
-WHERE evt.event_name NOT IN ('first_visit', 'purchase');
+WHERE evt.event_name NOT IN ("first_visit", "purchase");
